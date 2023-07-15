30e475915265c29a7d846aa4a532947e4dc9b4ea
kaurav83 <puga.vitaliy@gmail.com>
2023-07-15 23:38:10 +0300

    save loki

--- src/widgets/PageError/ui/PageError.stories.tsx
+++ src/widgets/PageError/ui/PageError.stories.tsx
@@ -5,7 +5,7 @@ import { Theme } from 'app/providers/ThemeProvider';
 import { PageError } from './PageError';
 
 export default {
-    title: 'widget/ErrorPage',
+    title: 'widgets/PageError',
     component: PageError,
     argTypes: {
         backgroundColor: { control: 'color' },
@@ -16,6 +16,7 @@ const Template: ComponentStory<typeof PageError> = (args) => <PageError {...args
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [ThemeDecorator(Theme.LIGHT)];
 
 export const Dark = Template.bind({});
 Dark.args = {};
