90f31097d6a62281569549658efba812e2d4c983
kaurav83 <puga.vitaliy@gmail.com>
2023-07-16 18:33:56 +0300

    removed some plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,20 +5,29 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 import { type BuildOptions } from './types/config'
 
-export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => ([
-  new HTMLWebpackPlugin({
-    template: paths.html
-  }),
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css'
-  }),
-  new webpack.DefinePlugin({
-    IS_DEV: JSON.stringify(isDev)
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: false,
-  }),
-])
+export const buildPlugins = ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    new HTMLWebpackPlugin({
+      template: paths.html
+    }),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    }),
+    new webpack.DefinePlugin({
+      IS_DEV: JSON.stringify(isDev)
+    }),
+  ]
+
+  if (isDev) {
+    plugins.push(
+      new webpack.HotModuleReplacementPlugin()
+    );
+    plugins.push(
+      new BundleAnalyzerPlugin({openAnalyzer: false})
+    );
+  }
+
+  return plugins;
+};
