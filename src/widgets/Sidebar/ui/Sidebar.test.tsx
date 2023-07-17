08a2ae1b9f34837c81e72c2ac286d5abc241fed4
kaurav83 <puga.vitaliy@gmail.com>
2023-07-17 22:34:26 +0300

    after coverage test sidebar

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,16 +1,17 @@
 import { fireEvent, screen } from '@testing-library/react';
 
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation';
+import { renderWithTranslation } from 'shared/config/tests/renderWithTranslation';
+import { componentRender } from 'shared/config/tests/componentRender';
 
 describe('test sidebar', () => {
   test('test sidebar exists', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('test toggle sidebar', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('toggle-sidebar');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
